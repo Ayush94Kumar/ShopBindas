4930021aab0e92d6d282c354b5debb65b1ba3d5a
ayush76671kumar@gmail.com <ayushkumar94703@gmail.com>
2026-06-12 10:03:40 +0530

    Added Latest Collection

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import Home_front from '../components/Home_front'
+import LatestCollection from '../components/LatestCollection'
 const Home = () => {
   return (
     <div>
       <Home_front/>
+      <LatestCollection/>
     </div>
   )
 }
