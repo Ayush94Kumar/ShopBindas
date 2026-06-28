0a06e07ceaea60818c1008518f2c010870f2de34
ayush76671kumar@gmail.com <ayushkumar94703@gmail.com>
2026-06-28 13:53:57 +0530

    Added listProduct removeProduct singleProduct API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -41,7 +41,7 @@ const addProduct = async (req, res) => {
         const product = new productModel(productData);
         await product.save()
 
-        res.json({success:true,message:"product added"});
+        res.json({ success: true, message: "product added" });
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: error.message })
@@ -50,18 +50,42 @@ const addProduct = async (req, res) => {
 
 //function for list product
 const listProduct = async (req, res) => {
+    try {
+        const products = await productModel.find({});
+        res.json({success:true,products})
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
 
 //function for remove product
 const removeProduct = async (req, res) => {
+    try {
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success:true,message:"Product Delete"})
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
 // const single product information
 
 const singleProduct = async (req, res) => {
+    try {
+        const {productId} = req.body
+        const product= await productModel.findById(productId);
+        res.json({success:true,product})
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
