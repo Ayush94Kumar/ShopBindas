3a20d9434e80e2e933c457725b5d1de3840b024a
ayush76671kumar@gmail.com <ayushkumar94703@gmail.com>
2026-07-01 16:57:46 +0530

    Added Admin Login logic

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
        <div className='flex items-center justify-between py-5 font medium'>
                 <img src={assets.company_logo} className="w-12 " alt="company_Logo" />
                 <img src={assets.logo} className="w-36" alt="Logo" />
             </div>
-      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm font-serif cursor-pointer'>Logout</button>
+      <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm font-serif cursor-pointer'>Logout</button>
     </div>
   )
 }
